44d2feb7e506045ed6b5221338d0e8e90ce2d279
John Sandfort <garnavis@gmail.com>
2013-12-02 14:07:40 -0500

    Working on Ruby Day 3 homework. SystemStackError at my_csv_module.rb:43.

---
+++ ruby/my_csv_module.rb
@@ -0,0 +1,55 @@
+module ActsAsCsv
+  def self.included(base)
+    base.extend ClassMethods
+  end
+  
+  module ClassMethods
+    def acts_as_csv
+      include InstanceMethods
+    end
+  end
+  
+  module InstanceMethods   
+    def read
+      @csv_contents = []
+      filename = self.class.to_s.downcase + '.txt'
+      file = File.new(filename)
+      @headers = file.gets.chomp.split(', ')
+
+      file.each do |row|
+        @csv_contents << row.chomp.split(', ')
+      end
+    end
+
+    attr_accessor :headers, :csv_contents
+
+    def each(&block)
+        csv_contents.each {|row| yield CsvRow.new(headers, row)}
+    end
+
+    def initialize
+      read 
+    end
+  end
+end
+
+class CsvRow
+    def initialize(headers=[], row=[])
+        @headers = headers
+        @row = row
+    end
+
+    def method_missing name, *args
+        row[headers.index(name.to_s)]
+    end
+end
+
+class RubyCsv  # no inheritance! You can mix it in
+  include ActsAsCsv
+  acts_as_csv
+end
+
+puts 'declaring new ActsAsCsv...'
+m = RubyCsv.new
+puts 'printing second column...'
+m.each {|row| row.two}
